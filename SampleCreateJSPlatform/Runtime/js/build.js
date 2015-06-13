5070c0d28bfcadbb2db6f3e10d9591e9fb35b3e9
cjgammon <cj.gammon@gmail.com>
2015-06-13 01:46:11 -0500

    add build file for optimizing

---
+++ SampleCreateJSPlatform/Runtime/js/build.js
@@ -0,0 +1,12 @@
+({
+    paths: {
+		tweenlite: 'vendor/greensock/TweenLite',
+	    timelinelite: 'vendor/greensock/TimelineLite',
+	    tweenmax: 'vendor/greensock/TweenMax',
+	    timelinemax: 'vendor/greensock/TimelineMax',
+		snapplugin: 'vendor/greensock/plugins/SnapPlugin',
+        snap: 'vendor/snap.svg/dist/snap.svg'
+    },
+    name: "main",
+    out: "main.min.js"
+})
