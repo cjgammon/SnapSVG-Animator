31e79124ac2dc6ae7df5060b7bbf245d154823fb
CJ Gammon <gammon@adobe.com>
2015-07-23 00:38:14 -0500

    add almond and update build for minification

--- SnapSVGAnimatorPlatform/EclipseProject/ExtensionContent/SnapSVGAnimator/js/build.js
+++ SnapSVGAnimatorPlatform/EclipseProject/ExtensionContent/SnapSVGAnimator/js/build.js
@@ -1,12 +1,11 @@
 ({
+    baseUrl: './',
     paths: {
-		tweenlite: 'vendor/greensock/TweenLite',
-	    timelinelite: 'vendor/greensock/TimelineLite',
-	    tweenmax: 'vendor/greensock/TweenMax',
-	    timelinemax: 'vendor/greensock/TimelineMax',
-		snapplugin: 'vendor/greensock/plugins/SnapPlugin',
         snap: 'vendor/snap.svg/dist/snap.svg'
     },
-    name: "main",
+    findNestedDependencies: true,
+    optimize: 'uglify2',
+    include: ['main'],
+    name: "vendor/almond/almond",
     out: "main.min.js"
 })
